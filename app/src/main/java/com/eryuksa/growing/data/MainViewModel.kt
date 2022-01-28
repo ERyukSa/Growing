8b61545d27b7551d5a6deb60ea7905dbc88d26f3
SH_H264 <jslkap227@gmail.com>
2022-01-28 14:45:21 +0900

    Fix dataBinding error about custom setter in layout

--- app/src/main/java/com/eryuksa/growing/data/MainViewModel.kt
+++ app/src/main/java/com/eryuksa/growing/data/MainViewModel.kt
@@ -1,37 +1,29 @@
 package com.eryuksa.growing.data
 
-import android.view.MenuItem
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.eryuksa.growing.R
-import com.eryuksa.growing.fragment.miracle_morning.ui.MiracleMorningFragment
-import com.eryuksa.growing.fragment.motivation.MotivationFragment
-import com.eryuksa.growing.fragment.together.TogetherFragment
 
-class MainViewModel(private val fm: FragmentManager) : ViewModel() {
+private const val TAG = "MainViewModel"
 
-    // 바텀 네비게이션 뷰가 클릭되면, 데이터 바인딩으로 동작
-    fun setFragment(menuItem: MenuItem) {
-        val fragmentType = getFragmentType(menuItem.itemId)
-        var fragment = fm.findFragmentByTag(fragmentType.name)
-        val transaction = fm.beginTransaction()
+class MainViewModel : ViewModel() {
 
-        // 앱에 처음 진입하는 경우
-        if (fragment == null) {
-            fragment = when (fragmentType) {
-                MainFragmentType.TOGETHER -> TogetherFragment.newInstance()
-                MainFragmentType.MOTIVATION -> MotivationFragment.newInstance()
-                MainFragmentType.MIRACLE_MORNING -> MiracleMorningFragment.newInstance()
-            }
+    private val _currentFragmentType = MutableLiveData(MainFragmentType.MIRACLE_MORNING)
+
+    val currentFragmentType: LiveData<MainFragmentType>
+        get() = _currentFragmentType
+
+    // 현재 보여줄 프래그먼트 타입 변경
+    fun setFragmentType(menuItemId: Int): Boolean {
+        return try{
+            _currentFragmentType.value = getFragmentType(menuItemId)
+            true
+        } catch (e: java.lang.IllegalArgumentException) {
+            Log.e(TAG, e.message ?: "error occured in setFragmentType()")
+            false
         }
-
-        transaction
-            .add(R.id.fragment_container, fragment, fragmentType.name)
-            .show(fragment)
-        hideElseFragments(fragmentType, transaction) // 나머지 프래그먼트들을 숨긴다
-        transaction.commit()
     }
 
     // 바텀 메뉴 아이템에 맞게 프래그먼트 타입을 가져온다
@@ -43,27 +35,4 @@ class MainViewModel(private val fm: FragmentManager) : ViewModel() {
             else -> throw IllegalArgumentException("not found main fragment Id")
         }
     }
-
-    // 선택되지 않은 프래그먼트들을 숨긴다
-    private fun hideElseFragments(fragmentType: MainFragmentType, transaction: FragmentTransaction) {
-        MainFragmentType.values()
-            .filterNot { it == fragmentType }
-            .forEach {
-                val toHideFragment = fm.findFragmentByTag(it.name)
-                toHideFragment?.let {
-                    transaction.hide(toHideFragment)
-                }
-            }
-    }
-}
-
-class MainViewModelFactory(private val fm: FragmentManager) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            MainViewModel(fm) as T
-        } else {
-            throw IllegalArgumentException()
-        }
-    }
-
 }
--- app/src/main/java/com/eryuksa/growing/ui/MainActivity.kt
+++ app/src/main/java/com/eryuksa/growing/ui/MainActivity.kt
@@ -3,11 +3,15 @@ package com.eryuksa.growing.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import com.eryuksa.growing.R
+import com.eryuksa.growing.data.MainFragmentType
 import com.eryuksa.growing.data.MainViewModel
-import com.eryuksa.growing.data.MainViewModelFactory
 import com.eryuksa.growing.databinding.ActivityMainBinding
+import com.eryuksa.growing.fragment.miracle_morning.ui.MiracleMorningFragment
+import com.eryuksa.growing.fragment.motivation.MotivationFragment
+import com.eryuksa.growing.fragment.together.TogetherFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,19 +19,62 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
     private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(
-            this,
-            MainViewModelFactory(supportFragmentManager) // 프래그먼트 매니저를 인자로 전달
-        )[MainViewModel::class.java]
+        ViewModelProvider(this)[MainViewModel::class.java]
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         initBinding()
+        observeFragmentType()
     }
 
     private fun initBinding() {
-        binding.lifecycleOwner = this // binding에 LiveData 사용
         binding.viewModel = viewModel
+    }
+
+    // 프래그먼트 타입이 변경되면, 화면에 보여줄 프래그먼트를 변경한다
+    private fun observeFragmentType() {
+        viewModel.currentFragmentType.observe(this) {
+            setFragment(it)
+        }
+
+    }
+
+    // 선택된 프래그먼트로 변경
+    private fun setFragment(fragmentType: MainFragmentType) {
+        var fragment = supportFragmentManager.findFragmentByTag(fragmentType.name)
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // 앱에 처음 진입하는 경우
+        if (fragment == null) {
+            fragment = when (fragmentType) {
+                MainFragmentType.TOGETHER -> TogetherFragment.newInstance()
+                MainFragmentType.MOTIVATION -> MotivationFragment.newInstance()
+                MainFragmentType.MIRACLE_MORNING -> MiracleMorningFragment.newInstance()
+            }
+
+            // 프래그먼트 인스턴스를 처음 생성해서, FM의 프래그먼트 리스트에 추가한다
+            transaction.add(R.id.fragment_container, fragment, fragmentType.name)
+        }
+
+        transaction.show(fragment)
+        hideElseFragments(fragmentType, transaction) // 나머지 프래그먼트들을 숨긴다
+        transaction.commit()
+    }
+
+    // 선택되지 않은 프래그먼트들을 숨긴다
+    private fun hideElseFragments(
+        fragmentType: MainFragmentType,
+        transaction: FragmentTransaction
+    ) {
+        MainFragmentType.values()
+            .filterNot { it == fragmentType }
+            .forEach {
+                val toHideFragment = supportFragmentManager.findFragmentByTag(it.name)
+                toHideFragment?.let {
+                    transaction.hide(toHideFragment)
+                }
+            }
     }
 }
