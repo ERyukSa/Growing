cf97b96f3b7e3119f5365fe34096ee0e9dd683c8
SH_H264 <jslkap227@gmail.com>
2022-03-15 17:38:34 +0900

    add infinite scroll done
    - YoutubeListViewModel.kt
    
    무한 스크롤 적용)
    - chip을 빠르게 요리조리 클릭할 떄 에러나던 현상 수정
    how?
    - 카테고리 변경 로직을 동작시키기 전에 이전 작업 취소하는 로직 추가
     1. loadingJob 멤버 변수 도입 -> loadingJob.cancel()
     2. isLoading = false -> if(isLoading) return 문으로 인해 버튼 클릭이 무시되던 문제 수정

--- app/src/main/java/com/eryuksa/growing/motivation/YoutubeListViewModel.kt
+++ app/src/main/java/com/eryuksa/growing/motivation/YoutubeListViewModel.kt
@@ -9,7 +9,7 @@ import com.eryuksa.growing.R
 import com.eryuksa.growing.motivation.data.YoutubeCategory
 import com.eryuksa.growing.motivation.data.YoutubeItem
 import com.eryuksa.growing.motivation.data.YoutubeRepository
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class YoutubeListViewModel : ViewModel() {
@@ -18,29 +18,36 @@ class YoutubeListViewModel : ViewModel() {
 
     private var currentList = mutableListOf<YoutubeItem>()
     private var currentCategory = YoutubeCategory.ALL
-    private var lastLoadedItem: YoutubeItem? = null
+    private var lastLoadedItem: YoutubeItem? = null // DB의 cursor를 위한 변수. 리포지토리로 옮기는 게 더 좋을 것 같다는 생각이 든다
 
+    // 리스트가 변경됐을 때 데이터가 추가된 것인지, 카테고리가 변경된 것인지 View에서 구분하기 위함
+    // 이 부분도 ViewModel에서 다 처리할 수 있으면 더 좋을 것 같다.
     private var _categoryChanged = false
     val categoryChanged get() = _categoryChanged
 
+    private lateinit var loadingJob: Job // 리포지토리에 데이터 요청하는 코루틴
     private var isLoading = false
-    var prevListSize = 0
+    var prevListSize = 0 // 변경되기 전의 리스트 사이즈
 
     private val _youtubeList: MutableLiveData<List<YoutubeItem>> = MutableLiveData(emptyList())
     val youtubeList: LiveData<List<YoutubeItem>>
         get() = _youtubeList
 
-    init{
+    init {
         loadMoreItems()
     }
 
+    /**
+     * 리스트 아이템을 (더) 가져온다
+     * 빠르게 스크롤 할 때를 대비해서 isLoading 변수 사용
+     */
     fun loadMoreItems() {
         if (isLoading) return
 
         isLoading = true
-        prevListSize = currentList.size
+        prevListSize = currentList.size // Fragment에서 참조 -> 추가된 데이터 만큼만 notify
 
-        CoroutineScope(viewModelScope.coroutineContext).launch {
+        loadingJob = viewModelScope.launch {
             currentList.addAll(repository.getItems(currentCategory, lastLoadedItem))
             lastLoadedItem = currentList.last()
             _youtubeList.value = currentList
@@ -48,16 +55,29 @@ class YoutubeListViewModel : ViewModel() {
         }
     }
 
+    /**
+     * 이전에 요청했던 데이터 로딩 작업 중단하는 함수
+     * 여러 chip을 빠르게 클릭할 때를 대비한다.
+     */
+    private fun stopLoadingItems() {
+        loadingJob.cancel()
+        isLoading = false
+    }
+
     private fun changeCategory(category: YoutubeCategory) {
         currentList.clear()
-        _categoryChanged = true
-        _youtubeList.value = currentList
-        lastLoadedItem = null
+        _categoryChanged = true // 프래그먼트에서 참조 -> 카테고리가 바뀌었을 땐 전체 번경: notifyDataSetChanged()
+        _youtubeList.value = currentList // 빈 리스트 리사이클러 뷰에 반영
+        lastLoadedItem = null // 첫 번째 아이템부터 필요하다는 의미 -> 리포지토리에서 Firestore cursor를 첫번째 아이템으로 세팅
         currentCategory = category
         _categoryChanged = false
     }
 
     fun onChipCheckedChanged(@IdRes checkedId: Int) {
+        if (isLoading) {
+            stopLoadingItems()
+        }
+
         val category = getCategory(checkedId)
         changeCategory(category)
         loadMoreItems()
